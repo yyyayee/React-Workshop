3cfe24008952fe254b73c61501762f23b44884e8
yyyayee <tanida.yt@gmail.com>
2024-09-16 11:20:40 +0700

    quiz-app
    quiz-app

---
+++ quiz-app/src/App.js
@@ -0,0 +1,25 @@
+import {createContext, useState } from "react"
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
+
+import './App.css'
+
+export const DataContext = createContext()
+
+function App() {
+  const [appState, setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
+  return(
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>}
+        {appState === "quiz" && <Quiz/>}
+        {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+  );
+}
+
+export default App;
---
+++ quiz-app/src/component/Score.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
+import QuestionsData from "../data/QuestionsData";
+
+const Score = ()=>{
+    const {score,setAppState,setScore} = useContext(DataContext)
+
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
+
+    return(
+        <div className="score">
+          <h1>สรุปผลคะแนน</h1>
+          <h2>{score} / {QuestionsData.length}</h2>
+          <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
+        </div>
+    )
+}
+
+export default Score;
